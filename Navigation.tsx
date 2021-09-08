0d2b0698d3b38181e146e645651d95e89b3de4c1
dongwu-kim <alduk7797@naver.com>
2021-09-08 11:42:33 +0900

    초기세팅

---
+++ Navigation.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {App} from './app/presentation/component/App';
+
+const Stack = createNativeStackNavigator();
+
+export const Navigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="App"
+          component={App}
+          options={{title: 'Commuting App'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ index.js
@@ -0,0 +1,5 @@
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import {Navigation} from './Navigation';
+
+AppRegistry.registerComponent(appName, () => Navigation);
