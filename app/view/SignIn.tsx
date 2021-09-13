c70e0caf154be81f7f4ac4533b147aab2f3532f1
201steve <79638133+201steve@users.noreply.github.com>
2021-09-13 15:34:58 +0900

    [Steve] [ADD] 소셜 로그인 로직 (#13)

--- app/view/SignIn.tsx
+++ app/view/SignIn.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Image, VStack} from 'native-base';
 import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
+import {signInSocialAuth} from '../model/useCase/signInSocialAuth';
 
 export const SignIn = ({navigation}) => {
   const {googleButton} = style;
@@ -19,7 +20,9 @@ export const SignIn = ({navigation}) => {
         style={googleButton}
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Light}
-        onPress={() => {}}
+        onPress={() => {
+          signInSocialAuth();
+        }}
       />
     </VStack>
   );
