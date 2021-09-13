3948666164b35ec3f282ef40647813fdc2ba3f38
dongwu-kim <alduk7797@naver.com>
2021-09-13 14:17:34 +0900

    [Hugo] [ADD] : SignInValidation

--- app/presentation/components/signIn/SignIn.tsx
+++ app/presentation/components/signIn/SignIn.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Image, VStack} from 'native-base';
 import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
-import {signInSocialAuth} from '../model/useCase/signInSocialAuth';
+import {ISignInProps} from '../../interface/ISignIn';
 
-export const SignIn = ({navigation}) => {
+export const SignIn = ({handleSocialSignIn}: ISignInProps) => {
   const {googleButton} = style;
-  const {navigate} = navigation;
+
   return (
     <VStack flex={1} bg="white" justifyContent="center" alignItems="center">
       <Image
-        source={require('../../data/images/soomgo_logo_rgb.png')}
+        source={require('../../../../data/images/soomgo_logo_rgb.png')}
         alt="Logo"
         w="70%"
         resizeMode="contain"
@@ -20,9 +20,7 @@ export const SignIn = ({navigation}) => {
         style={googleButton}
         size={GoogleSigninButton.Size.Wide}
         color={GoogleSigninButton.Color.Light}
-        onPress={() => {
-          signInSocialAuth();
-        }}
+        onPress={handleSocialSignIn}
       />
     </VStack>
   );
---
+++ app/presentation/components/signIn/SignInPresenter.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import {SignInUseCase} from '../../../domain/useCase/SignInUseCase';
+import {useAccessUserEmail} from '../../hooks/signIn';
+import {ISignInUser} from '../../interface/ISignIn';
+
+import {SignIn} from './SignIn';
+
+const signInLogic = new SignInUseCase();
+const {signInValidation, signInGoogleAuth} = signInLogic;
+
+const handleSocialSignIn = (
+  setSignInUserInfo: React.Dispatch<React.SetStateAction<ISignInUser | null>>,
+): void => {
+  signInGoogleAuth().then(userInfoWithoutToken => {
+    setSignInUserInfo(userInfoWithoutToken);
+  });
+};
+
+export const SignInPresenter = ({navigation}: any) => {
+  const {navigate} = navigation;
+
+  const [accessUserEmail, accessUserLoading] = useAccessUserEmail();
+  const [signInUserInfo, setSignInUserInfo] = useState<ISignInUser | null>(
+    null,
+  );
+
+  useEffect(() => {
+    if (!accessUserLoading) {
+      if (signInUserInfo?.email) {
+        if (signInValidation(signInUserInfo.email, accessUserEmail)) {
+          navigate('Main');
+        }
+      }
+    }
+  }, [accessUserEmail, signInUserInfo]);
+
+  return (
+    <SignIn
+      handleSocialSignIn={() => {
+        handleSocialSignIn(setSignInUserInfo);
+      }}
+    />
+  );
+};
