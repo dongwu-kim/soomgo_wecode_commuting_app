b2aece87a50795d8042b7042b9dcf4fc71368301
dongwu-kim <alduk7797@naver.com>
2021-09-16 10:29:46 +0900

    [Hugo] [MODIFY] : WorkLog Load logic & SaveOrInsert button toggle

--- app/data/repository/WorkLogRepository.ts
+++ app/data/repository/WorkLogRepository.ts
@@ -5,30 +5,33 @@ import {IWorkLog} from '../../presentation/interface/IWorkLog';
 export class WorkLogRepository extends UsingFirebaseDB {
   async getYesterdayWorkLog(): Promise<IWorkLog | null> {
     try {
-      const workLog = await super.getDataFromDB(
-        `/uid/task/${yesterdayYearMonthDate()}`,
-        'value',
-        snapshot => {
-          return {...snapshot.val()};
-        },
-      );
+      const workLog = await super.getDataFromDB(`/uid/task/${yesterdayYearMonthDate()}`, 'value', snapshot => {
+        return {...snapshot.val()};
+      });
       return workLog;
     } catch {
+      console.log('yesterdayWorkLog loading Error!');
       return null;
     }
   }
   async getTodayWorkLog(): Promise<IWorkLog | null> {
     try {
-      const workLog = await super.getDataFromDB(
-        `/uid/task/${todayYearMonthDate()}`,
-        'value',
-        snapshot => {
-          return {...snapshot.val()};
-        },
-      );
+      const workLog = await super.getDataFromDB(`/uid/task/${todayYearMonthDate()}`, 'value', snapshot => {
+        return {...snapshot.val()};
+      });
       return workLog;
     } catch {
+      console.log('todayWorkLog loading Error!');
       return null;
+    }
+  }
+
+  async setWorkLogInDB(yesterdayLogText: string, todayWorkLogText: string) {
+    try {
+      await super.setDataToDB(`/uid/task/${yesterdayYearMonthDate()}/workLog`, yesterdayLogText);
+      await super.setDataToDB(`/uid/task/${todayYearMonthDate()}/workLog`, todayWorkLogText);
+    } catch {
+      console.log('DB update Error! : WorkLog');
     }
   }
 }
