0dd7a055c9c331b9985388201de1f638843bc965
mekemeke94 <79459557+mekemeke94@users.noreply.github.com>
2021-09-23 20:14:55 +0900

    [Milo] [ADD] bottom tab navigator (#32)
    Co-authored-by: parktaehwan <parktaehwan@bagtaehwan-ui-MacBookAir.local>

--- Navigation.tsx
+++ Navigation.tsx
@@ -5,8 +5,32 @@ import {NativeBaseProvider} from 'native-base';
 import {SignInPresenter} from './app/presentation/components/signIn/SignInPresenter';
 import {WorkLogPresenter} from './app/presentation/components/workLog/WorkLogPresenter';
 import {MainPresenter} from './app/presentation/components/main/MainPresenter';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {Meeting} from './app/presentation/components/meeting/Meeting';
+import {MyPage} from './app/presentation/components/myPage/MyPage';
 
 const Stack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStackScreen = () => {
+  return (
+    <Stack.Navigator>
+      <HomeStack.Screen name="Main" component={MainPresenter} options={{headerShown: false}} />
+      <HomeStack.Screen name="WorkLog" component={WorkLogPresenter} />
+    </Stack.Navigator>
+  );
+};
+
+const MainTabScreen = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen name="Home" component={HomeStackScreen} options={{headerShown: false}} />
+      <Tab.Screen name="Meeting" component={Meeting} />
+      <Tab.Screen name="MyPage" component={MyPage} />
+    </Tab.Navigator>
+  );
+};
 
 export const Navigation = () => {
   return (
@@ -14,8 +38,7 @@ export const Navigation = () => {
       <NativeBaseProvider>
         <Stack.Navigator>
           <Stack.Screen name="SignIn" component={SignInPresenter} options={{headerShown: false}} />
-          <Stack.Screen name="Main" component={MainPresenter} options={{headerShown: false}} />
-          <Stack.Screen name="WorkLog" component={WorkLogPresenter} />
+          <Stack.Screen name="Main" component={MainTabScreen} options={{headerShown: false}} />
         </Stack.Navigator>
       </NativeBaseProvider>
     </NavigationContainer>
---
+++ app/presentation/components/meeting/Meeting.tsx
@@ -0,0 +1,11 @@
+import {Text} from 'native-base';
+import React from 'react';
+import {View} from 'react-native';
+
+export const Meeting = () => {
+  return (
+    <View>
+      <Text>미팅</Text>
+    </View>
+  );
+};
