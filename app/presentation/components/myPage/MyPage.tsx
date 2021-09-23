151058826067768927025d493eb1ccea00e06acb
parktaehwan <parktaehwan@bagtaehwan-ui-MacBookAir.local>
2021-09-23 20:01:51 +0900

    Add: bottom tab navigator

---
+++ app/presentation/components/myPage/MyPage.tsx
@@ -0,0 +1,11 @@
+import {Text} from 'native-base';
+import React from 'react';
+import {View} from 'react-native';
+
+export const MyPage = () => {
+  return (
+    <View>
+      <Text>마이페이지</Text>
+    </View>
+  );
+};
