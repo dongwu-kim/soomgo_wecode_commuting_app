afcc84e2d8098e21c262a14fd987ec6edc89cfc9
dongwu-kim <74899114+dongwu-kim@users.noreply.github.com>
2021-09-24 16:38:12 +0900

    [Hugo] [MODIFY] : Task Layout (#38)

--- app/presentation/components/workLog/WorkLog.tsx
+++ app/presentation/components/workLog/WorkLog.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {VStack, Box, Button, Heading, TextArea} from 'native-base';
 import {IWorkLogProps} from '../../interface/IWorkLog';
+import {Platform} from 'react-native';
 
 export const WorkLog = ({
   yesterdayWorkLogText,
@@ -13,14 +14,19 @@ export const WorkLog = ({
   saveWorkLog,
 }: IWorkLogProps) => {
   return (
-    <VStack>
-      <Box my={2}>
-        <Heading mx={2}>어제 한 일</Heading>
+    <VStack alignItems="center" bgColor="warmGray.200">
+      <Box mt={8} pt={3} w="95%" bgColor="white" borderWidth={1} borderColor="coolGray.300" borderRadius={10}>
+        <Heading fontSize="xl" mx={2}>
+          어제 한 일
+        </Heading>
         <TextArea
           h={40}
           mx={2}
           my={4}
-          border={1}
+          borderWidth={1}
+          borderColor="coolGray.300"
+          borderRadius={15}
+          _focus={{borderColor: '#00c7ae'}}
           placeholder="최소 30자, 최대 200자로 입력 해주세요."
           value={yesterdayWorkLogText}
           onChangeText={e => {
@@ -29,13 +35,18 @@ export const WorkLog = ({
           }}
         />
       </Box>
-      <Box my={2}>
-        <Heading mx={2}>오늘 할 일</Heading>
+      <Box mt={8} pt={3} w="95%" bgColor="white" borderWidth={1} borderColor="coolGray.300" borderRadius={10}>
+        <Heading fontSize="xl" mx={2}>
+          오늘 할 일
+        </Heading>
         <TextArea
           h={40}
           mx={2}
           my={4}
-          border={1}
+          borderWidth={1}
+          borderColor="coolGray.300"
+          borderRadius={10}
+          _focus={{borderColor: '#00c7ae'}}
           placeholder="최소 30자, 최대 200자로 입력 해주세요."
           value={todayWorkLogText}
           onChangeText={e => {
@@ -44,8 +55,15 @@ export const WorkLog = ({
           }}
         />
       </Box>
-      <Box>
-        <Button position="relative" mt={20} isDisabled={saveButtonDisabled} onPress={saveWorkLog}>
+      <Box w="100%" alignItems="center" justifyContent="center">
+        <Button
+          w="90%"
+          mt={Platform.OS === 'ios' ? 8 : 5}
+          isDisabled={saveButtonDisabled}
+          onPress={saveWorkLog}
+          backgroundColor="#00c7ae"
+          _pressed={{backgroundColor: '#5dd6c6'}}
+          _disabled={{backgroundColor: '#9de7dd'}}>
           {insertCheck ? '수정하기' : '저장하기'}
         </Button>
       </Box>
