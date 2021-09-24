4c20dad77790ba9a67f6274dcb1e99f8c3f7d052
mekemeke94 <79459557+mekemeke94@users.noreply.github.com>
2021-09-24 16:47:18 +0900

    [Milo] [MODIFY]: main, bottom-tab layout 수정 (#39)
    Co-authored-by: parktaehwan <parktaehwan@bagtaehwan-ui-MacBookAir.local>

--- app/presentation/components/main/Main.tsx
+++ app/presentation/components/main/Main.tsx
@@ -34,9 +34,16 @@ export const Main = ({
     progressColor = 'warning';
   }
   return (
-    <SafeAreaView>
-      <VStack justifyContent="center" alignItems="center" _ios={{mt: '10'}}>
-        <Box w="100%" justifyContent="center" alignItems="center" py={2} borderBottomWidth={1}>
+    <SafeAreaView style={{backgroundColor: 'white'}}>
+      <VStack justifyContent="center" alignItems="center" backgroundColor="warmGray.200" _ios={{mt: '10'}}>
+        <Box
+          w="100%"
+          justifyContent="center"
+          alignItems="center"
+          py={2}
+          bgColor="white"
+          borderColor="gray.300"
+          borderBottomWidth={1}>
           <Image
             source={require('../../../../data/images/soomgo_logo_rgb.png')}
             alt="Logo"
@@ -46,90 +53,114 @@ export const Main = ({
             px={1}
           />
         </Box>
-        <Box w="90%" marginTop={5} marginBottom={3}>
-          <Text fontSize="xl" fontWeight={600}>
-            안녕하세요, {userName}님
-          </Text>
-        </Box>
-        <Box w="90%" borderLeftWidth={5} borderColor="gray.400" paddingLeft={3}>
-          <Box flexDirection="row">
-            <Text marginBottom={1} fontSize="md" fontWeight={600}>
-              {dayjsNow().slice(0, 10)},
-            </Text>
-            <Text marginLeft={3} fontSize="md" fontWeight={600}>
-              {'현재 위치 : ' + address}
-            </Text>
-          </Box>
-          <Box>
-            <Text fontSize="md" fontWeight={600}>
-              {loadWorkTimeLog
-                ? `${loadWorkTimeLog[0]} ${loadWorkTimeLog[1] !== loadWorkTimeLog[0] ? '~' + loadWorkTimeLog[1] : ''}`
-                : '10:00 AM ~ 07:00 PM'}
-            </Text>
-          </Box>
-        </Box>
-        <Button
-          w="90%"
-          mt={5}
-          py={4}
-          backgroundColor="#00c7ae"
+        <VStack
+          w="95%"
           borderWidth={1}
-          onPress={() => {
-            setTimeStamp(nowMilliSec());
-            workBtn && navigate('WorkLog');
-          }}
-          _text={Platform.OS === 'ios' ? {fontSize: '3xl', color: 'white'} : {fontSize: 'xl', color: 'white'}}
-          disabled={commuteButtonDisabled !== null ? commuteButtonDisabled : false}>
-          {workBtn ? '출근하기' : '퇴근하기'}
-        </Button>
-        <Stack w="90%" mt={2}>
+          borderColor="gray.300"
+          borderRadius={5}
+          bgColor="white"
+          py={1}
+          alignItems="center"
+          mt={2}>
+          <Box w="90%" marginTop={3} marginBottom={3}>
+            <Text fontSize="xl" fontWeight={600}>
+              안녕하세요, {userName}님
+            </Text>
+          </Box>
+          <Box w="90%" borderLeftWidth={5} borderColor="gray.400" paddingLeft={3}>
+            <Box flexDirection="row">
+              <Text marginBottom={1} fontSize="md" fontWeight={600}>
+                {dayjsNow().slice(0, 10)},
+              </Text>
+              <Text marginLeft={3} fontSize="md" fontWeight={600}>
+                {'현재 위치 : ' + address}
+              </Text>
+            </Box>
+            <Box>
+              <Text fontSize="md" fontWeight={600}>
+                {loadWorkTimeLog
+                  ? `${loadWorkTimeLog[0]} ${loadWorkTimeLog[1] !== loadWorkTimeLog[0] ? '~' + loadWorkTimeLog[1] : ''}`
+                  : '10:00 AM ~ 07:00 PM'}
+              </Text>
+            </Box>
+          </Box>
           <Button
-            py={2}
-            bg="white"
+            w="90%"
+            mt={3}
+            py={4}
+            backgroundColor="#00c7ae"
             borderWidth={1}
-            _text={Platform.OS === 'ios' ? {fontWeight: 600, fontSize: 'xl'} : {fontWeight: 600, fontSize: 'lg'}}
+            _pressed={{backgroundColor: '#5DD6C6'}}
+            _disabled={{backgroundColor: '#9DE7DD'}}
             onPress={() => {
-              navigate('WorkTimeDetail');
-            }}>
-            출퇴근 기록 확인하기
+              setTimeStamp(nowMilliSec());
+              workBtn && navigate('WorkLog');
+            }}
+            _text={Platform.OS === 'ios' ? {fontSize: '3xl', color: 'white'} : {fontSize: 'xl', color: 'white'}}
+            disabled={commuteButtonDisabled !== null ? commuteButtonDisabled : false}>
+            {workBtn ? '출근하기' : '퇴근하기'}
           </Button>
-          <Button
-            py={2}
-            bg="white"
-            borderWidth={1}
-            mt={1}
-            _text={Platform.OS === 'ios' ? {fontWeight: 600, fontSize: 'xl'} : {fontWeight: 600, fontSize: 'lg'}}
-            onPress={() => {
-              navigate('WorkLog');
-            }}>
-            근무일지 작성
-          </Button>
-        </Stack>
+          <Stack w="90%" mt={2}>
+            <Button
+              py={2}
+              bg="white"
+              borderWidth={1}
+              _pressed={{backgroundColor: '#5DD6C6'}}
+              _disabled={{backgroundColor: '#9DE7DD'}}
+              _text={Platform.OS === 'ios' ? {fontWeight: 600, fontSize: 'xl'} : {fontWeight: 600, fontSize: 'lg'}}
+              onPress={() => {
+                navigate('WorkTimeDetail');
+              }}>
+              출퇴근 기록 확인하기
+            </Button>
+            <Button
+              py={2}
+              bg="white"
+              borderWidth={1}
+              mt={1}
+              mb={3}
+              _pressed={{backgroundColor: '#5DD6C6'}}
+              _disabled={{backgroundColor: '#9DE7DD'}}
+              _text={Platform.OS === 'ios' ? {fontWeight: 600, fontSize: 'xl'} : {fontWeight: 600, fontSize: 'lg'}}
+              onPress={() => {
+                navigate('WorkLog');
+              }}>
+              근무일지 작성
+            </Button>
+          </Stack>
+        </VStack>
         <MainWorkThisWeek
           weekWorkLog={weekWorkLog}
           weekWorkHourMinute={weekWorkHourMinute}
           weekWorkTimeProgressPercent={weekWorkTimeProgressPercent}
         />
-        <Box
-          flexDirection="row"
-          justifyContent="space-between"
+        <VStack
+          w="95%"
+          borderWidth={1}
+          borderColor="gray.300"
+          borderRadius={5}
+          bgColor="white"
+          py={1}
           alignItems="center"
-          w="90%"
-          mt={Platform.OS === 'ios' ? 5 : 3}>
-          <Text fontSize={'2xl'}>일 평균 근로시간 </Text>
-          <Text
-            pr={3}
-            fontSize={'md'}
-            onPress={() => {
-              navigate('DatePicker', {otherParam: 'Main'});
-            }}>
-            {`${startMonth + '-' + startDay} ~ ${endMonth + '-' + endDay}`}
-          </Text>
-        </Box>
-        <Progress value={workTimeAverageNum} w="90%" mt={5} mb={2} colorScheme={progressColor} />
-        <Box w="80%" mb={5} _text={{textAlign: 'right', fontSize: 'xl', fontWeight: 600}}>
-          {workTimeAverage}
-        </Box>
+          mt={2}>
+          <Box flexDirection="row" justifyContent="space-between" alignItems="center" w="90%" mt={3}>
+            <Text fontSize={Platform.OS === 'ios' ? '2xl' : 'lg'} fontWeight={600}>
+              일 평균 근로시간
+            </Text>
+            <Text
+              pr={3}
+              fontSize={'md'}
+              onPress={() => {
+                navigate('DatePicker', {otherParam: 'Main'});
+              }}>
+              {`${startMonth + '-' + startDay} ~ ${endMonth + '-' + endDay}`}
+            </Text>
+          </Box>
+          <Progress value={workTimeAverageNum} w="90%" mt={5} mb={2} colorScheme={progressColor} />
+          <Box w="80%" mb={3} _text={{textAlign: 'right', fontSize: 'lg', fontWeight: 600}}>
+            {workTimeAverage}
+          </Box>
+        </VStack>
       </VStack>
     </SafeAreaView>
   );
