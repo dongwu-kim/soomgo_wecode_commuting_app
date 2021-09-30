a17f4eb2d404b5ae20db266af15fd1afe5219163
dongwu-kim <74899114+dongwu-kim@users.noreply.github.com>
2021-09-30 16:53:26 +0900

    [Hugo] [ADD] : SignInRepository test logic (#58)

---
+++ app/data/repository/signIn/SignInRepository.test.ts
@@ -0,0 +1,37 @@
+import {SignInRepository} from './SignInRepository';
+import UsingFirebaseDB from '../UsingFirebaseDB';
+
+jest.mock('@react-native-firebase/auth', () => () => ({
+  currentUser: {
+    uid: 'mn4KAHT6GceKRLP3XeCFdeGHMRC2',
+  },
+}));
+
+jest.mock('@react-native-firebase/database', () => () => ({
+  ref: jest.fn(refDir => ({
+    once: jest.fn(async () => ({
+      val: () => {
+        return ['email@email.com'];
+      },
+    })),
+    set: jest.fn(value => {
+      return {refDir, value};
+    }),
+  })),
+}));
+
+describe('[SignInRepository Test]', () => {
+  const {getAccessUserEmailFromDB, setUserInfoToDB} = new SignInRepository();
+
+  test('[SignInRepository Test] : getAccessUserEmailFromDB test', () => {
+    getAccessUserEmailFromDB().then(accessEmailArray => {
+      expect(accessEmailArray).toEqual(['email@email.com']);
+    });
+  });
+
+  test('[SignInRepository Test] : setUserInfoToDB test', () => {
+    const setDBMethodCall = jest.spyOn(UsingFirebaseDB.prototype, 'setDataToDB').mockImplementation(async () => {});
+    setUserInfoToDB({userInfo: 'userInfo'});
+    expect(setDBMethodCall).toBeCalledTimes(1);
+  });
+});
